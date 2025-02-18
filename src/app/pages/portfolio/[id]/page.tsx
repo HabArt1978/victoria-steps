4da673d2a5ff600dd4dfb211bf02c56a9eaf73b8
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2025-02-18 04:43:31 +0300

    WIP - params id fixed warnings

--- src/app/pages/portfolio/[id]/page.tsx
+++ src/app/pages/portfolio/[id]/page.tsx
@@ -3,15 +3,15 @@ import SectionContainer from '@/components/Containers/SectionContainer'
 import { H1 } from '@/components/UI/Typography'
 import type { JSX } from 'react'
 
-interface PageParams {
-  params: {
+interface SingleWorkProps {
+  params: Promise<{
     id: string
-  }
+  }>
 }
 
-const SingleWorkPage = async ({
-  params: { id }
-}: PageParams): Promise<JSX.Element> => {
+const SingleWorkPage = async (props: SingleWorkProps): Promise<JSX.Element> => {
+  const { id } = await props.params
+
   return (
     <PageContainer>
       <SectionContainer>
--- src/library/dataForComponents/ourWorksData/ourWorksData.ts
+++ src/library/dataForComponents/ourWorksData/ourWorksData.ts
@@ -1,97 +1,96 @@
 import ourWorkZeroImage from '@/assets/images/our_works.webp'
 import ourWorkFirsImage from '@/assets/images/our_works_1.webp'
 import ourWorkSecondImage from '@/assets/images/our_works_2.webp'
-import { nanoid } from 'nanoid'
 
 export const ourWorksData = [
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-1'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-2'
   },
   {
     image: ourWorkSecondImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-3'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-4'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-5'
   },
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-6'
   },
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-7'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-8'
   },
   {
     image: ourWorkSecondImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-9'
   },
   {
     image: ourWorkSecondImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-10'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-11'
   },
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-12'
   },
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-13'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-14'
   },
   {
     image: ourWorkSecondImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-15'
   },
   {
     image: ourWorkSecondImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-16'
   },
   {
     image: ourWorkFirsImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-17'
   },
   {
     image: ourWorkZeroImage,
     alt: 'Изображение лестницы с нашими ступенями',
-    id: nanoid()
+    id: 'work-18'
   }
 ]
