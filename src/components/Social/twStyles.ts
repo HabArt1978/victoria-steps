2e006049e4470ec968383f534cf467558182a011
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2025-02-19 02:30:48 +0300

    WIP - fixed bg-color for Social links

--- src/components/Social/twStyles.ts
+++ src/components/Social/twStyles.ts
@@ -2,7 +2,7 @@ import { cn } from '@/utils/cn'
 
 export const socialLink = (isHorizontalPosition: boolean): string =>
   cn(
-    'group btn btn-circle btn-lg m-0 border-none bg-black bg-opacity-40 hover:rotate-12 hover:scale-125 hover:bg-transparent transition duration-200',
+    'group btn btn-circle btn-lg m-0 border-none hover:rotate-12 bg-[rgba(0,0,0,0.4)] hover:scale-125 hover:bg-transparent transition duration-200',
 
     { 'bg-inherit': isHorizontalPosition }
   )
