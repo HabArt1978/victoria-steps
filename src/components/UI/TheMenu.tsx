17111c918203424ce4cf969e6def90f0c4177bfa
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2025-02-25 13:49:59 +0300

    WIP - icon change when button status changes

--- src/components/UI/TheMenu.tsx
+++ src/components/UI/TheMenu.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/utils/cn'
 import { useEffect, useState, type JSX } from 'react'
-import { FiMenu } from 'react-icons/fi'
+import { FiMenu, FiX } from 'react-icons/fi'
 import { useMediaQuery } from 'usehooks-ts'
 
 const TheMenu = (): JSX.Element => {
@@ -57,7 +57,13 @@ const TheMenu = (): JSX.Element => {
         onClick={() => clickHandler()}
         className="btn btn-circle btn-md fixed right-4 top-4 z-50 border-none bg-gradient-to-r from-yellow-400 to-red-600 bg-[length:200%_200%] transition-all duration-300 ease-in-out hover:from-red-600 hover:to-yellow-400 hover:bg-[length:100%_100%] xmd:hidden"
       >
-        <FiMenu className="h-6 w-6 text-white" />
+        <div className="transition duration-500">
+          {!isOpen ? (
+            <FiMenu className="h-6 w-6 text-white" />
+          ) : (
+            <FiX className="h-7 w-7 text-white" />
+          )}
+        </div>
       </button>
     </>
   )
