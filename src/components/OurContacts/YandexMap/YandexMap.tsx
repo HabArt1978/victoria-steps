f1201195a8232ac5558d5fdaef58a11fcd514fe2
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2025-03-18 06:09:41 +0300

    27 - toggle card status for page scrolling via the card widget

--- src/components/OurContacts/YandexMap/YandexMap.tsx
+++ src/components/OurContacts/YandexMap/YandexMap.tsx
@@ -1,7 +1,13 @@
+'use client'
+
+import { cn } from '@/utils/cn'
 import Link from 'next/link'
-import type { JSX } from 'react'
+import { useState, type JSX } from 'react'
+import { FaLock, FaUnlock } from 'react-icons/fa'
 
 const YandexMap = (): JSX.Element => {
+  const [isMapActive, setIsMapActive] = useState(false)
+
   return (
     <div className="relative h-full w-full overflow-hidden">
       <Link
@@ -16,12 +22,45 @@ const YandexMap = (): JSX.Element => {
       >
         Северо-Восточное шоссе, 12 — Яндекс Карты
       </Link>
-      <iframe
-        src="https://yandex.ru/map-widget/v1/?from=tableau_yabro&ll=45.242876%2C54.207400&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNDQzMDY1NTUxEnPQoNC-0YHRgdC40Y8sINCg0LXRgdC_0YPQsdC70LjQutCwINCc0L7RgNC00L7QstC40Y8sINCh0LDRgNCw0L3RgdC6LCDQodC10LLQtdGA0L4t0JLQvtGB0YLQvtGH0L3QvtC1INGI0L7RgdGB0LUsIDEyIgoNyPc0QhXT1FhC&z=16.09"
-        allowFullScreen={true}
-        className="relative h-full w-full"
-        loading="lazy"
-      ></iframe>
+
+      <div className="relative h-full w-full">
+        {/* Overlay, который блокирует взаимодействие с картой */}
+        {!isMapActive && (
+          <div className="absolute inset-0 z-10 bg-transparent" />
+        )}
+
+        {/* Кнопка для активации/деактивации карты */}
+
+        <button
+          className={cn(
+            'btn btn-outline btn-xs absolute left-2 top-2 z-20 h-fit w-52 rounded bg-green-200 px-4 py-2 font-robotoMono font-normal uppercase tracking-wider text-gray-800 shadow-lg',
+            'hover:bg-green-300',
+            {
+              'bg-orange-300 hover:bg-orange-400': isMapActive
+            }
+          )}
+          onClick={() => setIsMapActive(!isMapActive)}
+        >
+          <span>
+            {isMapActive ? (
+              <FaUnlock className="size-4" />
+            ) : (
+              <FaLock className="size-4" />
+            )}
+          </span>
+          <span>
+            {' '}
+            {isMapActive ? 'Заблокировать карту' : 'Активировать карту'}
+          </span>
+        </button>
+
+        <iframe
+          src="https://yandex.ru/map-widget/v1/?from=tableau_yabro&ll=45.242876%2C54.207400&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgoxNDQzMDY1NTUxEnPQoNC-0YHRgdC40Y8sINCg0LXRgdC_0YPQsdC70LjQutCwINCc0L7RgNC00L7QstC40Y8sINCh0LDRgNCw0L3RgdC6LCDQodC10LLQtdGA0L4t0JLQvtGB0YLQvtGH0L3QvtC1INGI0L7RgdGB0LUsIDEyIgoNyPc0QhXT1FhC&z=16.09"
+          allowFullScreen={true}
+          className="relative h-full w-full"
+          loading="lazy"
+        ></iframe>
+      </div>
     </div>
   )
 }
